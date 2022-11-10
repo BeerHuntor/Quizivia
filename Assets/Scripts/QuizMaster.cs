92d43f6339064dcfb5dd87b57d0eb857b5a8b3b9
HuntorGames <rt001a9138@gmail.com>
2022-11-10 13:38:23 +0000

    Refactor and decoupling of UI elements
    Removed all UI element methods and moved them to their own UIManager class.  Commented code to enable future understanding

--- Assets/Scripts/QuizMaster.cs
+++ Assets/Scripts/QuizMaster.cs
@@ -4,23 +4,30 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
+///<summary>
+/// Handles all logic of the quiz, checking answers, keeping score and initialising the quiz setup. 
+///</summary>
+
 public class QuizMaster : MonoBehaviour
 {
-    [SerializeField] TextMeshProUGUI questionText;
-    [SerializeField] Button[] answerButtons = new Button[4];
+    UIManager _uiManager; 
     List<Question> myQuestions;
     private int currentQuestion;
-    private string chosenAnswer;
     private int quizScore;
     private int pointsPerQuestionCorrect;
     private const int QuizSize = 2; // Set to a setting later - so user can chose amount of questions. 
 
     void Start()
     {
+        _uiManager = FindObjectOfType<UIManager>();
         GenerateQuiz();
         NextQuestion();
     }
 
+    ///<summary>
+    /// Generates a new quiz with the desired size of questions and calls to grab the question list from the Quiz object.
+    /// Initialises values needed for a new quiz, score, currentQuestionIndex etc
+    ///</summary>
     void GenerateQuiz()
     {
         Quiz myQuiz = new Quiz(QuizSize);
@@ -29,26 +36,33 @@ public class QuizMaster : MonoBehaviour
         currentQuestion = 0;
         myQuestions = myQuiz.GetQuestions();
     }
+
+    ///<summary>
+    /// Gets the current question number to pass to other methods. 
+    ///</summary>
     private int GetCurrentQuestionNumber()
     {
         return currentQuestion;
     }
-    /* Triggers the OnPointerClick Event on the chosen answer button. */
-    public void OnClicked(Button buttonClicked)
+
+    ///<summary>
+    /// Checks the selected answer against the correct answer of the question and handles the response. 
+    ///</summary>
+    public void CheckAnswer(string answer)
     {
-        chosenAnswer = buttonClicked.gameObject.GetComponentInChildren<TextMeshProUGUI>().text;
-    }
-    /* Checks the answer selected against the correct answer */
-    public void CheckAnswer()
-    {
-        if (chosenAnswer == myQuestions[GetCurrentQuestionNumber()].Correct_Answer)
+        if (answer == myQuestions[GetCurrentQuestionNumber()].Correct_Answer)
         {
             AddScore(pointsPerQuestionCorrect);
+            Debug.Log("Correct!");
         }
         currentQuestion++;
         NextQuestion();
     }
 
+    ///<summary>
+    /// Grabs the details of the next question to be shown and passes them off to the UIManager to display.
+    /// Also checks to see if we have run out of questions in the quiz and calls GameOver.
+    ///</summary>
     private void NextQuestion()
     {
         int qNum = GetCurrentQuestionNumber();
@@ -59,18 +73,22 @@ public class QuizMaster : MonoBehaviour
         }
         else
         {
-            questionText.text = myQuestions[qNum].QuizQuestion;
-            for (int i = 0; i < answerButtons.Length; i++)
-            {
-                answerButtons[i].GetComponentInChildren<TextMeshProUGUI>().text = myQuestions[qNum].QuestionAnswers[i];
-            }
+            _uiManager.ChangeQuestionText(myQuestions[qNum].QuizQuestion);
+            _uiManager.ChangeAnswerText(myQuestions[qNum].QuestionAnswers);
         }
     }
 
+    ///<summary>
+    /// Adds to the quizScore by the amount passed into the argument
+    ///</summary>
     private void AddScore(int scoreToAdd)
     {
         quizScore += scoreToAdd;
     }
+
+    ///<summary>
+    /// Returns the current quizScore
+    ///</summary>
     public int GetScore()
     {
         return quizScore;
